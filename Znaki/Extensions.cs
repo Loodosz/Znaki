d7a5443f1b2b68d145b32dc6d99fe303d7f6c6bf
Grzegorz Szałkowski <gszalkowski@sygnity.pl>
2021-02-18 16:09:13 +0100

    * created VS solution for the project * some code refactoring

---
+++ Znaki/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Znaki.Extensions
+{
+    public static class Extensions
+    {
+        public static bool IsNumber(this string text)
+        {
+            return int.TryParse(text, out _);
+        }
+
+        public static int AsNumber(this string text)
+        {
+            return int.Parse(text);
+        }
+    }
+}
